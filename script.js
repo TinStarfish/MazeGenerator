037add8fdff92e657df0618cdc9bbd42ee061a46
TinStarfish <Waterstrevor68@yahoo.com>
2018-10-05 14:27:06 -0600

    New Changes
    In class on Friday

--- script.js
+++ script.js
@@ -5,9 +5,18 @@ var canvas;
 var ctx;
 var grid;
 
-var MAZE_WIDTH = 400;
-var MAZE_HEIGHT = 400;
+var MAZE_WIDTH = 500;
+var MAZE_HEIGHT = 500;
 var BLOCK_SIZE = 20;
+
+var WALL_COLOR = 'rgb(47,52,64)';
+var PATH_COLOR = 'rgb(63,86,102)';
+var START_COLOR = 'rgb(248,202,77)';
+var END_COLOR = 'rgb(181,74,53)';
+var FOLLOW_COLOR = 'rgb(245,229,192)';
+
+var StartPoint;
+var EndPoint;
 
 
 function Main () {
@@ -27,9 +36,9 @@ function DrawCanvas() {
     
 }
 
-function DrawSquare(x,y) {
-    ctx.fillStyle = 'rgb(255,0,255)';
-    ctx.fillRect (x,y,BLOCK_SIZE,BLOCK_SIZE);
+function DrawSquare(x,y, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect (x,y,BLOCK_SIZE-1,BLOCK_SIZE-1);
     
 
     
@@ -38,6 +47,7 @@ function DrawSquare(x,y) {
 }
 
 function DrawMaze() {
+    //CREATE BASE FOR MASE
     for (var i = 0; i < MAZE_WIDTH; i++) {
         grid[i] = [];
         for (var j = 0; j < MAZE_HEIGHT; j++) {
@@ -46,15 +56,103 @@ function DrawMaze() {
         }
         
     }
+    
+    //CREATE START AND END POINTS RANDOMLY
+    SetNodes();
+    DrawStartAndEndPoints();
+   
     for(var y = 0; y<MAZE_HEIGHT;y++) {
         for(var x=0; x<MAZE_WIDTH;x++) {
+    //DRAW WALLS
             if(grid[x][y] == 1) {
-                DrawSquare(x*BLOCK_SIZE,y*BLOCK_SIZE);
+                DrawSquare(x*BLOCK_SIZE,y*BLOCK_SIZE, WALL_COLOR);
             }
+    //DRAW PATH
+            if(grid[x][y] == 0) {
+                DrawSquare(x*BLOCK_SIZE,y*BLOCK_SIZE, PATH_COLOR);
+                
+            }
+    //DRAW START
+            if(grid[x][y] == 2) {
+                DrawSquare(x*BLOCK_SIZE,y*BLOCK_SIZE, START_COLOR);
+                
+            }
+    //DRAW END
+            if(grid[x][y] == 3) {
+                DrawSquare(x*BLOCK_SIZE,y*BLOCK_SIZE, END_COLOR);
+                
+            }
+            
             
         }
         
     }
+    
+    
+}
+
+function SetNodes() {
+    for(var y = 0; y<MAZE_HEIGHT-2;y++) {
+        for(var x=0; x<MAZE_WIDTH-2;x++) {
+            if(x%2!=0 && y%2!=0) {
+                grid[x][y] = 0;
+                
+                
+            }       
+            
+        }
+        
+    }
+    
+}
+
+function DrawStartAndEndPoints() {
+    var randSide = Math.floor(Math.random()*4);
+    var boolean = true;
+    var randX;
+    var randY;
+    
+    while(boolean) {
+    
+        randX = Math.floor(Math.random()*23);
+        randY = Math.floor(Math.random()*23); 
+        
+        if(randX%2==0 || randY%2==0) {
+            boolean = true;
+            
+        } else {
+            
+            boolean = false;
+        }
+        
+        
+    }
+    
+   
+    
+    
+    if(randSide==0) {
+        grid[1][randY] = 2;
+        grid[23][randY] = 3;
+        
+    }
+    if(randSide==1) {
+        grid[randX][1] = 2;
+        grid[randX][23] = 3;
+        
+    }
+    if(randSide==2) {
+        grid[randX][1] = 2;
+        grid[randX][23] = 3;
+        
+    }
+    if(randSide==3) {
+        grid[randX][1] = 2;
+        grid[randX][23] = 3;
+        
+    }
+    
+    
     
     
 }
