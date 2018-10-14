eacaeadfc34a226021d94abe100eb7c463e2df1b
TinStarfish <Waterstrevor68@yahoo.com>
2018-10-14 09:59:29 -0600

    Neighbors Push
    Figured out iterative neighbors as well as most of the maze. Need to figure out how to check the visited nodes

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ var MAZE_WIDTH = 500;
 var MAZE_HEIGHT = 500;
 var BLOCK_SIZE = 20;
 
+var MAZE_TEMP_WIDTH = 25;
+var MAZE_TEMP_HEIGHT = 25;
+
 var WALL_COLOR = 'rgb(47,52,64)';
 var PATH_COLOR = 'rgb(63,86,102)';
 var START_COLOR = 'rgb(248,202,77)';
@@ -17,14 +20,20 @@ var FOLLOW_COLOR = 'rgb(245,229,192)';
 
 var StartPoint;
 var EndPoint;
+var currentNode;
+var currentNode_X;
+var currentNode_Y;
 
 var Visited = [];
 var Traceback = [];
 
 function Main () {
     DrawCanvas();
-    DrawMaze();
+    MazeBase();
+    SetNodes();
+    DrawStartAndEndPoints();
     DepthFirstSearchIterative();
+    DrawMaze();
     
 }
 
@@ -43,29 +52,29 @@ function DrawSquare(x,y, color) {
     ctx.fillStyle = color;
     ctx.fillRect (x,y,BLOCK_SIZE-1,BLOCK_SIZE-1);
     
-
-    
-    
     
 }
 
-function DrawMaze() {
-    //CREATE BASE FOR MASE
-    for (var i = 0; i < MAZE_WIDTH; i++) {
+function MazeBase() {
+        //CREATE BASE FOR MASE
+    for (var i = 0; i < MAZE_TEMP_WIDTH; i++) {
         grid[i] = [];
-        for (var j = 0; j < MAZE_HEIGHT; j++) {
+        for (var j = 0; j < MAZE_TEMP_HEIGHT; j++) {
             grid[i][j] = 1;
             
         }
         
     }
     
+}
+
+function DrawMaze() {
+
     //CREATE START AND END POINTS RANDOMLY
-    SetNodes();
-    DrawStartAndEndPoints();
+
    
-    for(var y = 0; y<MAZE_HEIGHT;y++) {
-        for(var x=0; x<MAZE_WIDTH;x++) {
+    for(var y = 0; y<MAZE_TEMP_HEIGHT;y++) {
+        for(var x=0; x<MAZE_TEMP_WIDTH;x++) {
     //DRAW WALLS
             if(grid[x][y] == 1) {
                 DrawSquare(x*BLOCK_SIZE,y*BLOCK_SIZE, WALL_COLOR);
@@ -85,6 +94,11 @@ function DrawMaze() {
                 DrawSquare(x*BLOCK_SIZE,y*BLOCK_SIZE, END_COLOR);
                 
             }
+    //DRAW FOLLOW
+            if(grid[x][y] == 4) {
+                DrawSquare(x*BLOCK_SIZE,y*BLOCK_SIZE, FOLLOW_COLOR);
+                
+            }
             
             
         }
@@ -95,8 +109,8 @@ function DrawMaze() {
 }
 
 function SetNodes() {
-    for(var y = 0; y<MAZE_HEIGHT-2;y++) {
-        for(var x=0; x<MAZE_WIDTH-2;x++) {
+    for(var y = 0; y<MAZE_TEMP_HEIGHT-1;y++) {
+        for(var x=0; x<MAZE_TEMP_WIDTH-1;x++) {
             if(x%2!=0 && y%2!=0) {
                 grid[x][y] = 0;
                 
@@ -109,26 +123,142 @@ function SetNodes() {
     
 }
 
-//function FindNearestNeighbors(){
+function FindNearestNeighbors(){
 
 
 
-//}
+}
 
 function DepthFirstSearchIterative() {
+    Visited = [currentNode_X,currentNode_Y];
+    //Visited.push(currentNode_X,currentNode_Y);
+    Visited.push(3);
     
-    Visited.push(StartPoint);
     Traceback.push(StartPoint);
     console.log(Visited);
     
+    var nextNode;
+
     
+    currentNode = Visited[0];
     
+    for(var i = 0; i < MAZE_TEMP_HEIGHT*MAZE_TEMP_WIDTH;i++) {
+            var randomDir = Math.floor(Math.random()*4);
+        
+            if(randomDir==0){
+                 if(currentNode_X-2>1) {
+                    for(var x = 0; x < Visited.length; x++){
+                            if(grid[currentNode_X-2][currentNode_Y]==Visited[x]){
+                                console.log("It's already added, mi amigo");
+                                
+                                break;
+
+                            } else {
+                                currentNode_X = currentNode_X-2;
+                                grid[currentNode_X][currentNode_Y] = 4;
+                                
+                                
+                                grid[currentNode_X+1][currentNode_Y] = 4;
+                                
+                                console.log("Cur X:" + currentNode_X + "Cur Y:" + currentNode_Y);
+                                
+                            }
+                        
+                    }
+                    
+                     
+                 } else {
+                    console.log("'Round the outside");
+                
+                 }
+                
+                
+            }
+        if(randomDir==1){
+                 if(currentNode_X+2<MAZE_TEMP_HEIGHT-2){
+                    for(var x = 0; x < Visited.length; x++){
+                            if(grid[currentNode_X+2][currentNode_Y]==Visited[x]){
+                                console.log("It's already added, mi amigo");
+                                break;
+                                
+                            } else {
+                                currentNode_X = currentNode_X+2;
+                                grid[currentNode_X][currentNode_Y] = 4;
+                                grid[currentNode_X-1][currentNode_Y] = 4;
+                              
+                                
+                            }
+                        
+                    }
+                    
+                     
+                 } else {
+                    
+                
+                 }
+                
+                
+            }
+        if(randomDir==2){
+                 if(currentNode_Y-2>1){
+                    for(var x = 0; x < Visited.length; x++){
+                            if(grid[currentNode_X][currentNode_Y-2]==Visited[i]){
+                                console.log("It's already added, mi amigo");
+                                break;
+                                
+                            } else {
+                                currentNode_Y = currentNode_Y-2;
+                                grid[currentNode_X][currentNode_Y] = 4; //set node to path
+                                grid[currentNode_X][currentNode_Y+1] = 4; //set wall to path
+
+        
+                                
+                            }
+                        
+                    }
+                    
+                     
+                 } else {
+                    console.log("YOU DUN F*CKED UP");
+                
+                 }
+                
+                
+            }
+            
+        if(randomDir==3){
+                 if(currentNode_X+2<MAZE_TEMP_WIDTH-2){
+                    for(var x = 0; x < Visited.length; x++){
+                            if(grid[currentNode_X][currentNode_Y+2]==Visited[i]){
+                                console.log("It's already added, mi amigo");
+                                break;
+                            } else {
+                                currentNode_Y = currentNode_Y+2;
+                                grid[currentNode_X][currentNode_Y] = 4; //set node to path
+                                grid[currentNode_X][currentNode_Y-1] = 4; //set wall to path
+                                
+                                
+                            }
+                        
+                    }
+                   
+                     
+                 } else {
+                    console.log("YOU DUN F*CKED UP");
+                
+                 }
+                
+                
+            } 
+                    
+        
+    }    
     
 }
 
 function DrawStartAndEndPoints() {
     var randSide = Math.floor(Math.random()*4);
-    var boolean = true;
+    console.log("random side: " + randSide);
     var randX;
     var randY;
     /*
@@ -137,23 +267,26 @@ function DrawStartAndEndPoints() {
     randX = c;
     */
     if(randSide==0 || randSide==2){
-        randX = Math.floor(Math.random(MAZE_WIDTH-1)/2) * 2 + 1;
+        randX = (Math.floor(Math.random()*(MAZE_TEMP_WIDTH-1)/2))*2+1;
         randY = 1;
         grid[randX][randY] = 2;
+        StartPoint = grid[randX][randY];
+        currentNode_X = randX;
+        currentNode_Y = randY;
+        console.log("Ran:"+randSide + " X:" + randX + " Y:" + randY);
+    
         
     }
-    if(randSide==1 || randSide==3){
+    else if(randSide==1 || randSide==3){
         randX = 1;
-        randY = Math.floor(Math.random(MAZE_HEIGHT-1)/2) * 2 + 1;
+        randY = (Math.floor(Math.random()*(MAZE_TEMP_HEIGHT-1)/2))*2+1;
         grid[randX][randY] = 2;
-        
-        
+        StartPoint = grid[randX][randY];
+        currentNode_X = randX;
+        currentNode_Y = randY;
+        console.log("Ran:"+randSide + " X:" + randX + " Y:" + randY);
+         
     }
-    
-    
-    
-    
-    
 }
 
 Main();
